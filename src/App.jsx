37d0c5f1ff7f07bac30d8e11a02c1616b64e7a4f
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-06-21 16:51:12 -0500

    Feature decrement, incrementByAmount in counterSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import logo from './logo.svg'
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
-import { increment } from './store/slices/counter/counterSlice';
+import { decrement, increment, incrementByAmount } from './store/slices/counter/counterSlice';
 
 function App() {
   
   const { counter } = useSelector( state => state.counter)
+
   const  dispatch = useDispatch();
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>count is: {counter}</p>
         <p>
           <button type="button" onClick={() => dispatch( increment())}>
-            count is: {counter}
+            Increment
+          </button>
+          <button type="button" onClick={() => dispatch( decrement())}>
+            Decrement
+          </button>
+          <button type="button" onClick={() => dispatch( incrementByAmount(2))}>
+            Increment by 2
           </button>
         </p>
       </header>
